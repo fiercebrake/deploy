ea7afe731172ab7f7c93a3f855b2c4235deeb3dc
David Valiente <fierce.brake@gmail.com>
2025-06-28 15:01:27 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -4,7 +4,7 @@ sudo docker create --name arch --rm --tty --interactive --privileged -e PS1="ADC
 sudo docker start arch
 
 sudo docker exec arch pacman-key --init && pacman-key --populate
-sudo docker exec arch pacman -Syu --needed --noconfirm git curl wget vim nginx sudo screen cronie openssh linux linux-headers
+sudo docker exec arch pacman -Syu --needed --noconfirm git curl wget vim nginx sudo screen cronie openssh
 sudo docker exec arch ln -s /usr/bin/vim /usr/bin/vi
 sudo docker exec arch bash -c "echo -e 'repo  ALL=(ALL:ALL) ALL\nrepo ALL=(ALL) NOPASSWD: ALL
 ' > /etc/sudoers.d/repo" 
@@ -15,4 +15,4 @@ sudo docker exec arch bash -c "git clone https://github.com/fiercebrake/tkg.git 
 sudo docker exec arch bash -c "chown -R repo:repo /mnt/tkg/"
 sudo docker exec arch bash -c "/usr/bin/nginx &"
 sudo docker exec arch bash -c "chmod +x /mnt/tkg/linuxtkg.sh"
-# sudo docker exec arch bash -c "cd /mnt/tkg/ && sudo -u repo ./linuxtkg.sh"
+sudo docker exec arch bash -c "cd /mnt/tkg/ && sudo -u repo ./linuxtkg.sh"
